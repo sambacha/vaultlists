e0159124e95bb939e2b7591b293783b4a15ce04f
sam <sam@freighttrust.com>
2021-02-02 03:45:29 -0800

    refactor(schema): import yearn tools api schema

---
+++ bin/ipfs.js
@@ -0,0 +1,22 @@
+/**
+* @title Given a URI that may be ipfs, ipns, http, or https protocol
+* @function: return the fetch-able http(s) URLs for the same content
+* @param: uri to convert to fetch-able http url
+*/
+export default function uriToHttp(uri: string): string[] {
+  const protocol = uri.split(':')[0].toLowerCase()
+  switch (protocol) {
+    case 'https':
+      return [uri]
+    case 'http':
+      return [uri]
+    case 'ipfs':
+      const hash = uri.match(/^ipfs:(\/\/)?(.*)$/i)?.[2]
+      return [`https://cloudflare-ipfs.com/ipfs/${hash}/`, `https://ipfs.io/ipfs/${hash}/`]
+    case 'ipns':
+      const name = uri.match(/^ipns:(\/\/)?(.*)$/i)?.[2]
+      return [`https://cloudflare-ipfs.com/ipns/${name}/`, `https://ipfs.io/ipns/${name}/`]
+    default:
+      return []
+  }
+}
